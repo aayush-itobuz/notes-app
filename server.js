7038fb0940311a9f5edd1a21faeb725466129ab3
Aayush Kumar Gupta <aayush@itobuz.com>
2025-02-04 18:34:05 +0530

    feat: seed done

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import express from 'express';
-import dotenv from 'dotenv/config'
+import 'dotenv/config';
 import userRoute from './routes/userRoute.js';
 import noteRoute from './routes/noteRoute.js';
 import mongoConnect from './config/dbConnection.js';
-// import fs from 'fs'
 
 const app = express();
 
 const port = process.env.PORT;
-
-// const dir = "./uploads"
-// if(!fs.existsSync(dir)) {
-//   fs.mkdirSync(dir)
-// }
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
